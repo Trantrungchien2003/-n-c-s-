482760ce4f9665e56154fb3c8579790f6ac9f99a
chien.220250@tbd.edu.vn <chien.220250@tbd.edu.vn>
2025-04-12 12:59:39 +0000

    Created LoginForm, SignupForm and MainForm; Edited settings; Deleted Form1

--- client_code/MainForm/__init__.py
+++ client_code/MainForm/__init__.py
@@ -1,8 +1,12 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import MainFormTemplate
 from anvil import *
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
 
 
-class Form1(Form1Template):
+class MainForm(MainFormTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
